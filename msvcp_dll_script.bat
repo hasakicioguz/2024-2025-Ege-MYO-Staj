dd69fe74c88ad48efa4a9c1d7ca62ec0e70bd68c
Oğuz Efe Peker <122752042+hasakicioguz@users.noreply.github.com>
2025-10-11 19:57:40 +0300

    Sil msvcp_dll_script.bat

--- msvcp_dll_script.bat
+++
@@ -1,8 +0,0 @@
-net stop wuauserv
-net stop cryptSvc
-net stop bits
-net stop msiserver
-ren C:\Windows\SoftwareDistribution SoftwareDistribution.old
-regsvr32 /u msvcp140.dll
-regsvr32 msvcp140.dll
-net start wuauserv
